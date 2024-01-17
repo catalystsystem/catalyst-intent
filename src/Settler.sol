19819468ff265a2cb11fc101006cc94f8cbb76d1
Alexander <alexander@lindgren.xyz>
2024-01-17 15:59:35 +0100

    feat: more structure

--- src/Settler.sol
+++ src/Settler.sol
@@ -1,14 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.22;
+
+import { OrderDescription, Signature } from "./interfaces/Structs.sol";
 
 contract Settler {
-    uint256 public number;
+    error IncorrectSourceChain(bytes32 actual, bytes32 order);
+    error OrderTimedOut(uint64 timestamp, uint64 orderTimeout);
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    bytes32 immutable SOURCE_CHAIN;
+
+    constructor(bytes32 source_chain) {
+        SOURCE_CHAIN = source_chain;
     }
 
-    function increment() public {
-        number++;
+
+    function _getOrderHash(OrderDescription calldata order) internal pure returns(bytes32 orderHash) {
+        return orderHash = keccak256(abi.encodePacked(
+            order.destinationChain,
+            order.destinationAsset,
+            order.sourceChain,
+            order.sourceAsset,
+            order.minBond,
+            order.timeout,
+            order.evaluationContract,
+            order.evaluationContext
+        ));
+    }
+
+    function getOrderHash(OrderDescription calldata order) external pure returns(bytes32 orderHash) {
+        return orderHash = _getOrderHash(order);
+    }
+
+    function _getOrderOwner(bytes32 orderHash, Signature calldata signature) internal pure returns (address orderOwner) {
+        return orderOwner = ecrecover(orderHash, signature.v, signature.r, signature.s);
+    }
+
+    function getOrderOwner(bytes32 orderHash, Signature calldata signature) external pure returns(address orderOwner) {
+        return orderOwner = _getOrderOwner(orderHash, signature);
+    }
+    
+    function claimOrder(OrderDescription calldata order, Signature calldata signature) external {
+        // Check that this is the appropiate source chain.
+        if (SOURCE_CHAIN != order.sourceChain) revert IncorrectSourceChain(SOURCE_CHAIN, order.source_chain);
+        // Check that the order hasn't expired.
+        if (uint64(block.timestamp) > order.timeout) revert OrderTimedOut(uint64(block.timestamp), order.timeout);
+
+        // Get order hash so we can check that the owner is correctly provided.
+        bytes32 orderHash = _getOrderHash(order);
+        // Get the order owner.
+        address orderOwner = _getOrderOwner(orderHash, signature);
+
+        
+
     }
 }
---
+++ src/interfaces/Structs.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+struct OrderDescription {
+    bytes32 destinationChain;
+    bytes destinationAsset; // bytes65
+    bytes32 sourceChain;
+    address sourceAsset;
+    uint256 minBond;
+    uint64 timeout;
+    bytes32 evaluationContract;
+    bytes evaluationContext;
+}
+
+struct OrderContext {
+    address claimer;
+    uint256 sourceAssetsClaimed;
+    address sourceAsset
+}
+
+struct Signature {
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+}

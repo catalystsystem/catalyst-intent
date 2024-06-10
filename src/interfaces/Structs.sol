31bc73db6a760571ad66bce495bcb45210041618
Alexander <git@lindgren.xyz>
2024-06-10 11:22:16 +0200

    feat: order key & docs

--- src/interfaces/Structs.sol
+++ src/interfaces/Structs.sol
@@ -1,16 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
+import { Input, Output } from "./ISettlementContract.sol";
+
 //////////////////
 // Order types ///
 //////////////////
 
+/**
+ * @notice The progressive order status of cross-chain orders
+ *
+ * - Unfilled: Default state. Progresses to Claimed.
+ * - Claimed: A solver has claimed this order. Progresses to Challanged or OPFilled.
+ * - Challenged: A claimed order was challenged. Progresses to Fraud or Proven.
+ * - Fraud: The solver did not prove delivery & Challenger claiemd the collateral. Final.
+ * - OPFilled: A claimed order was not challenged. Payed inputs to solver. Final.
+ * - Proven: A solver proved settlement. Progresses to Filled.
+ * - Filled: A proven settlement was paid. Final.
+ */
 enum OrderStatus {
     Unfilled,
     Claimed,
     Challenged,
     Fraud,
     OPFilled,
+    Proven,
     Filled
 }
 
@@ -47,7 +61,13 @@ struct OrderKey {
     bytes32 destinationAsset; // TODO: Is this a waste? Can we use this better?
     bytes32 destinationAddress; // TODO bytes? For better future compatability?
     uint256 amount;
+
+    bytes inputs; // abi.encode(Input[])
+    // Lets say the challanger maps keccak256(abi.encode(outputs)) => keccak256(OrderKey).
+    // Then we can easily check if these outputs have all been matched.
+    bytes outputs; // abi.encode(output[])
 }
+
 
 ///////////////////
 // Reactor types //

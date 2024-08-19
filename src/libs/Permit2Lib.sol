5e6f360d5f968f2deb952c9d0155a5bceee69164
Alexander <git@lindgren.xyz>
2024-08-19 11:59:10 +0200

    chore: rought update for permit2 lib

--- src/libs/Permit2Lib.sol
+++ src/libs/Permit2Lib.sol
@@ -6,7 +6,7 @@ import { ISignatureTransfer } from "permit2/src/interfaces/ISignatureTransfer.so
 import { Input } from "../interfaces/ISettlementContract.sol";
 import { OrderKey } from "../interfaces/Structs.sol";
 
-/// @notice Gets the Permit2 context for an orderkey
+/** @notice Gets the Permit2 context for an orderkey */
 library Permit2Lib {
     /**
      * @notice Converts OrderKey into a PermitTransferFrom
@@ -22,42 +22,29 @@ library Permit2Lib {
             ISignatureTransfer.SignatureTransferDetails[] memory transferDetails
         )
     {
+        // Load the number of inputs. We need them to set the array size & to iterate through each
+        // convert the input struct into a transferDetails struct.
         uint256 numInputs = order.inputs.length;
         ISignatureTransfer.TokenPermissions[] memory permitted = new ISignatureTransfer.TokenPermissions[](numInputs);
         transferDetails = new ISignatureTransfer.SignatureTransferDetails[](numInputs);
 
+        // Iterate through each input.
         for (uint256 i; i < numInputs; ++i) {
             address token = order.inputs[i].token;
             uint256 amount = order.inputs[i].amount;
+            // TODO: Is permitted set correctly for dutch auctions?
+            // Set the allowance. This is the explicit max allowed amount approved by the user.
             permitted[i] = ISignatureTransfer.TokenPermissions({ token: token, amount: amount });
+            // Set our requested transfer. This has to be less than or equal to the allowance
             transferDetails[i] = ISignatureTransfer.SignatureTransferDetails({ to: to, requestedAmount: amount });
         }
 
-        // Probably should use a batch here.
+        // Always use a batch transfer from. This allows us to easily standardize
+        // token collections for multiple inputs.
         permitBatch = ISignatureTransfer.PermitBatchTransferFrom({
             permitted: permitted,
             nonce: order.nonce,
             deadline: order.reactorContext.fillByDeadline
         });
     }
-
-    /**
-     * @notice Convert OrderKey into a permit object
-     */
-    // TODO: Is this needed?
-    /* function transferDetails(Output[] memory outputs, address to)
-        internal
-        pure
-        returns (ISignatureTransfer.SignatureTransferDetails[] memory batchSignatureTransfer)
-    {
-        uint256 numOutputs = outputs.length;
-        batchSignatureTransfer = new ISignatureTransfer.SignatureTransferDetails[](numOutputs);
-        for (uint256 i; i < numOutputs; ++i) {
-            batchSignatureTransfer[i] = new ISignatureTransfer.SignatureTransferDetails({
-                to: to,
-                requestedAmount: outputs[i].amount
-            });
-        }
-        return batchSignatureTransfer;
-    } */
 }

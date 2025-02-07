bf9b08087b385d37e2082031013fbecc9428f701
Alexander <git@lindgren.xyz>
2025-02-07 19:09:58 +0100

    chore: remove unused function from bytes lib

--- src/libs/BytesLib.sol
+++ src/libs/BytesLib.sol
@@ -11,12 +11,4 @@ library BytesLib {
             res.length := calldataload(lengthPtr)
         }
     }
-
-    function touint32Array(bytes calldata _bytes, uint256 arg) internal pure returns (uint32[] calldata res) {
-        assembly {
-            let lengthPtr := add(_bytes.offset, calldataload(add(_bytes.offset, mul(0x20, arg))))
-            res.offset := add(lengthPtr, 0x20)
-            res.length := calldataload(lengthPtr)
-        }
-    }
 }

89d59db2ede62e1d338a17f8cc49fd700a8f51f8
Asem-Abdelhady <asemshawkey@gmail.com>
2025-03-07 14:01:13 +0200

    chore: format

--- test/oracle/wormhole/submit.t.sol
+++ test/oracle/wormhole/submit.t.sol
@@ -51,6 +51,7 @@ contract TestSubmitWormholeOracleProofs is Test {
 
     function test_fill_then_submit_W(address sender, uint256 amount, address recipient, bytes32 orderId, bytes32 solverIdentifier) external {
         vm.assume(solverIdentifier != bytes32(0));
+
         token.mint(sender, amount);
         vm.prank(sender);
         token.approve(address(filler), amount);
@@ -82,7 +83,9 @@ contract TestSubmitWormholeOracleProofs is Test {
         oracle.submit(address(filler), payloads);
     }
 
-    // This test is used to test receive against other chains
+    /**
+     * @dev This is used to test receive against SUI chain
+     */
     function test_fill_no_fuzz_output() public {
         uint256 amount = 100;
         bytes32 solverIdentifier = bytes32(uint256(uint160(makeAddr("solver"))));
@@ -104,44 +107,6 @@ contract TestSubmitWormholeOracleProofs is Test {
             fulfillmentContext: hex""
         });
 
-        /**
-        Order id for 
-            {
-                user: @0xab,
-                nonce: 0,
-                origin_chain_id: 100,
-                fill_deadline: 60000,
-                local_oracle: @0x1611edd9a9d42dbcd9ae773ffa22be0f6017b00590959dd5c767e4efcd34cd0b,
-                collateral_token: @0xe4d0bcbdc026b98a242f13e2761601107c90de400f0c24cdafea526abf201c26,
-                collateral_amount: 0,
-                initiate_deadline: 60000,
-                challenge_deadline: 120000,
-                input: 0x0::input_type::Input {
-                    token: @0xe4d0bcbdc026b98a242f13e2761601107c90de400f0c24cdafea526abf201c26,
-                    amount: 100
-                },
-                outputs: [
-                    0x0::output_description_type::OutputDescription {
-                    remote_oracle: 0x0::bytes32::Bytes32 {
-                        bytes: @0x2e234dae75c793f67a35089c9d99245e1c58470b
-                    },
-                    remote_filler: 0x0::bytes32::Bytes32 {
-                        bytes: @0xf62849f9a0b5bf2913b396098f7c7019b51a820a
-                    },
-                    chain_id: 31337,
-                    token: 0x0::bytes32::Bytes32 {
-                        bytes: @0x5991a2df15a8f6a256d3ec51e99254cd3fb576a9
-                    },
-                    amount: 100,
-                    recipient: 0x0::bytes32::Bytes32 {
-                        bytes: @0x6217c47ffa5eb3f3c92247fffe22ad998242c5
-                    },
-                    remote_call: [],
-                    fulfilment_context: []
-                    }
-                ]
-            }
-         */
         bytes32 orderId = hex"e58f15295d1c9e383c8d4dad01ee03cfa4448f0d4f52925fa957d6e76612bff9";
         vm.expectCall(address(token), abi.encodeWithSignature("transferFrom(address,address,uint256)", address(sender), recipient, amount));
         

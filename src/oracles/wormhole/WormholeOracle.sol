c14dc03935a6bd19321248f1da1edd4f09574745
Alexander <git@lindgren.xyz>
2025-03-07 20:51:18 +0100

    feat: check if incoming messages chains have been configured

--- src/oracles/wormhole/WormholeOracle.sol
+++ src/oracles/wormhole/WormholeOracle.sol
@@ -142,8 +142,9 @@ contract WormholeOracle is BaseOracle, WormholeVerifier, Ownable {
 
         // Map remoteMessagingProtocolChainIdentifier to canonical chain id. This ensures we use canonical ids.
         uint256 remoteChainId = _chainIdentifierToBlockChainId[remoteMessagingProtocolChainIdentifier];
+        if (remoteChainId == 0) revert ZeroValue();
+        
         uint256 numPayloads = payloadHashes.length;
-
         for (uint256 i; i < numPayloads; ++i) {
             // Store payload attestations;
             bytes32 payloadHash = payloadHashes[i];

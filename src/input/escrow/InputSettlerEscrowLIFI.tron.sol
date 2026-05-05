c90e764e5edeb24e0a435a8f08ac9541be32186f
Alexander <git@lindgren.xyz>
2026-05-05 12:33:10 +0200

    Set Tron pragma to 0.8.25

--- src/input/escrow/InputSettlerEscrowLIFI.tron.sol
+++ src/input/escrow/InputSettlerEscrowLIFI.tron.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.25;
 
 import { SafeTransferLibTron } from "../../libs/SafeTransferLib.tron.sol";
 import { InputSettlerEscrowLIFI } from "./InputSettlerEscrowLIFI.sol";
